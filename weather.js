d52f9673b000e9193fad5406bd5c5e1c478a7725
Natalie Bo <nataliabo@hotmail.co.uk>
2024-01-28 23:32:58 +0000

    feat:created event listener to be able submit the search form.

--- weather.js
+++ weather.js
@@ -1,2 +1,14 @@
 // console.log('js is loaded');
 
+//event listener to be aable to submit the form
+
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const city = document.getElementById('search-input').ariaValueMax.trim();
+    if (city) {
+        fetchWeatherData(city);
+        saveSearchHistory (city);
+        displaySearchHistory();
+    }
+});

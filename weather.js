bb827b4335c6b2aaeb4e12d0fee5669a9947a824
Natalie Bo <nataliabo@hotmail.co.uk>
2024-01-29 00:12:40 +0000

    feat:fetching 5 days forecast data function.

--- weather.js
+++ weather.js
@@ -40,3 +40,36 @@ function fetchCurrentWeather(city) {
         console.error('Error fetching current weather:', error);
     });
 }
+
+function updateCurrentWeather(data, city) {
+    const ukDate = new Date().toLocaleDateString('en-GB');
+    document.getElementById('city-name'.textContent = `Today in ${city}, ${country}`);
+    document.getElementById('current-date').textContent = ukDate;
+    document.getElementById('temperature').textContent = `Temperature: ${data.main.temp.toFixed(1)}°C`;
+    document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`;
+    document.getElementById('wind').textContent = `Wind: ${data.wind.speed.toFixed(1)} m/s`;
+    document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    document.getElementById('weather-icon').style.display = 'block';
+}
+
+//fetching the 5 days forecast
+function fetchForecast(city) {
+
+const apiKey = 'fb9672b35cbc85ae3baf12fecedeb034';
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Forecast data not found.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        updateForecastData(data);
+    })
+    .catch(error => {
+        console.error('Error fetching forecast data:', error);
+    });
+}
+
